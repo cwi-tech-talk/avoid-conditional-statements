6d6b578c2e0c652a4741d9b9dccbd7d7aa29866c
Róbson Machado Rosa <robsonrosa@cwi.com.br>
2017-12-07 00:18:52 -0200

    Criando assinatura de método a ser implementado no handson

---
+++ src/recomendacao-coop.js
@@ -0,0 +1,19 @@
+/**
+ * Sistema de recomendação de Jogos
+ * 
+ * Usuário informa algumas informações sobre seus interesses em jogos,
+ * então o sistema sugere um jogo que atenda ao gosto do usuário.
+ * 
+ * arg: interesses {
+ *   genero: 'aventura' | 'corrida' | 'fps' | 'esporte' | 'rpg' | 'estratégia',
+ *   experiencia: 'casual' | 'moderado' | 'hardcore',
+ *   ambiente: 'scifi' | 'medieval' | 'fantasia' | 'atual',
+ *   plataforma: 'pc' | 'ps' | 'xbox'
+ * }
+ * 
+ * returns [string] nome do jogo
+ */
+
+function recomendarJogo(interesse) {
+
+}
