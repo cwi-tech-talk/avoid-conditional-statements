bc1fbdd5d6a9925098fbff13019e6ec7e8579c95
Róbson Machado Rosa <robsonrosa@cwi.com.br>
2017-12-14 17:39:38 -0200

    Adicionando implementação colaborativa :)

--- src/recomendacao-coop.js
+++ src/recomendacao-coop.js
@@ -15,5 +15,47 @@
  */
 
 module.exports = function recomendarJogo(interesse) {
+    let  game;
+    switch(interesse.genero) {
+        case 'corrida':
+            game = (interesse.plataforma == 'xbox') ? 'Forza Motorsport' : 'TOP GEAR'
+            break;
+        case 'casuais':
+            game = 'Candy Crush'
+            break;
+         case 'estratégia':
+            game = 'Tactics Ogre'
+            break;    
+         case 'corrida':
+            break;
+        case 'mediavais hardcore':
+            if(interesse.plataforma == 'pc') {
+                game = "Dark Souls"
+            }
+            break;
+        case 'estratégia':
+            if(interesse.experiencia == 'hardcore' && interesse.ambiente == 'fantasia') {
+                game = 'Final Fantasy Tactics';
+            }
+            break;
+        default:
+            game = undefined;
+            break;
+    }
 
+    if(interesse.ambiente > 'medieval') {
+        game = 'The Loard Of The Rings'
+    } else if (interesse.ambiente === 'fantasia') {
+        if (interesse.plataforma === 'hardcore') {
+            game = 'Kingdom Hearts'
+        } else {
+            game = 'Chrono Trigger'
+        }
+    }
+
+    if(interesse.plataforma == 'pc') {
+        game = 'Team Fortress';
+    }
+
+    return game; 
 }
